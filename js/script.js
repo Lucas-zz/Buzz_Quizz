ed1b4a8db2cac98ea1e93bac595e50451ecdb89d
Lucas Azzolini Vieira <lucasazzollinivieira@gmail.com>
2021-11-22 17:38:00 -0300

    listing quizz

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 let data = [];
+let quizzData = [];
 let quizzId;
 
 function listQuizzes() {
@@ -13,16 +14,72 @@ function listQuizzes() {
 
         for (let i = 0; i < data.length; i++) {
 
+            let idQuizz = data[i].id;
+
             quizz.innerHTML +=
                 `
-                <article onclick="insideQuizz(${i})" data-identifier="quizz-card" data-identifier="general-quizzes">
-                <img src="${data[i].image}">
-                <span class="title">${data[i].title}</span>
-                <div class="gradient"></div>
+                <article onclick="insideQuizz(${idQuizz})" data-identifier="quizz-card" data-identifier="general-quizzes">
+                    <div class="gradient"></div>
+                    <img src="${data[i].image}">
+                    <span class="title">${data[i].title}</span>
                 </article>
                 `
+            idQuizz--;
         }
     });
+}
+
+function insideQuizz(id) {
+
+    quizzId = id;
+
+    let questions = [];
+    let levels = [];
+
+    const promise = axios.get('https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes/' + id);
+
+    promise.then((response) => {
+        console.log(response.data);
+        quizzData = response.data;
+
+        const main = document.querySelector(".listQuizz");
+        main.classList.add("noDisplay")
+
+        const quizz = document.querySelector(".insideQuizz");
+        quizz.classList.remove("noDisplay");
+
+        quizz.innerHTML = '';
+
+        quizz.innerHTML = `
+            <div class="insideHeader">
+                <div class="opacity"></div>
+                    <img src="${quizzData.image}">
+                <div class="center">
+                    <span class="insideTitle">${quizzData.title}</span>
+                </div>
+            </div>
+        `
+        let questData = quizzData.questions;
+        console.log(questData.length)
+
+        for (let i = 0; i < questData.length; i++) {
+
+            quizz.innerHTML += `
+            <section class="container">
+                <article class="margin">
+                    <div class="question" data-identifier="question">
+                        <span>${questData[i].title}</span>
+                    </div>
+                </article>
+            </section>
+            `
+            const quest = quizz.querySelector(".question");
+            quest.style.backgroundColor = questData[i].color;
+        }
+
+
+
+    })
 }
 
 // function insideQuizz() {
